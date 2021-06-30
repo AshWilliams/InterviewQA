e43d4b181a0025a0b3a1ef6bcf37f0baed4cdb58
Robert Rozas Navarro <rorozasn@microsoft.com>
2021-06-30 19:08:55 -0400

    seolution 2

--- Answers/Sol2.js
+++ Answers/Sol2.js
@@ -1,7 +1,9 @@
 let rotate = (arr) => {
     let len = arr.length
     let last = arr[len-1]
+    //pop las element
     arr.pop()
+    //insert last at array 1st position
     arr.splice(0,0,last)
 
     return arr
@@ -11,10 +13,13 @@ let rotate = (arr) => {
 function solution(A, K) {
     // write your code in JavaScript (Node.js 8.9.4)
     let len = A.length
+    //check emnpty array
+    if(len == 0){
+        return A
+    }
     let idx = 0
     for(; idx < K; idx++){
         A = rotate(A)
-    }   
-
+    } 
     return A
 }
